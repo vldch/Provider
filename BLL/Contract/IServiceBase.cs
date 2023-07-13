aed6abf6d3b913dede239bc8ff19ce155887a724
Vladimir <Vladimir@DESKTOP-MHR2N3M>
2023-07-13 08:48:25 +0300

    Add project files.

---
+++ BLL/Contract/IServiceBase.cs
@@ -0,0 +1,14 @@
+﻿using Provider.DAL.Entities;
+
+namespace Provider.BLL.Contract
+{
+    public interface IServiceBase<T>
+        where T : EntityBase
+    {
+        void Create(T entity);
+        void Delete(long id);
+        T Get(long id);
+        void Update(T entity);
+    }
+
+}
---
+++ DAL/Entities/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Provider.DAL.Entities
+{
+    public class EntityBase
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        [Key]
+        public long Id { get; set; }
+    }
+
+}
